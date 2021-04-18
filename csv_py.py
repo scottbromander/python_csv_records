5445dc1a0dca6c839e9d02450a9af278503a13f0
Scott Bromander <scottbromander@gmail.com>
2021-04-17 22:13:53 -0500

    sets columns

--- csv_py.py
+++ csv_py.py
@@ -8,3 +8,14 @@ Column = namedtuple('Column', 'src dest convert')
 
 def parse_timestamp(text):
     return datetime.strptime(text, '%Y-%m-%d %H:%M:%S')
+
+columns = [
+    Column('VendorID', 'vendor_id', int),
+    Column('passenger_count', 'num_passengers', int),
+    Column('tip_amount', 'tip', float),
+    Column('total_amount', 'price', float),
+    Column('tpep_dropoff_datetime', 'dropoff_time', parse_timestamp),
+    Column('tpep_pickup_datetime', 'pickup_time', parse_timestamp),
+    Column('trip_distance', 'distance', float),
+]
+
